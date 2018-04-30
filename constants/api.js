e607e2d0f781f1d67b45d023677837894ab3e89a
manojmishra571980 <manojmishra571980@gmail.com>
2018-04-30 14:09:34 +1000

    HomeScreen import working to app.js

--- constants/api.js
+++ constants/api.js
@@ -1,3 +1,21 @@
-export const fetchMeetups = () =>
-  fetch('http://192.168.0.4:3000/api/meetups')
-  .then(res =>  res.json() );
+//export const fetchMeetups = () =>
+ // fetch('http://10.133.44.111:3000/api/meetups')
+ // .then(res =>  res.json() );
+
+import axios from 'axios';
+//import { Platform } from 'react-native';
+
+axios.defaults.baseURL= 'http://10.133.44.111:3000/api';
+//axios.defaults.baseURL = url;
+const fakeGroupId = '5ae54364b713353104815402';
+class MeetupApi 
+{  constructor() {  this.groupId = fakeGroupId;
+                    this.path = `/groups/${this.groupId}/meetups`;
+                 }
+   async fetchGroupMeetups() 
+   {     const { data } = await axios.get(this.path);
+                 return data.meetups;
+         
+    }
+}
+export { MeetupApi,};
---
+++ src/screens/home/HomeScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+//import styles from './styles/HomeScreen';
+
+
+class HomeScreen extends Component {
+ 
+  render() {
+    return (
+      <View >
+
+          <Text>HomeScreen</Text>
+
+      </View>
+    );
+  }
+}
+
+export default HomeScreen;
