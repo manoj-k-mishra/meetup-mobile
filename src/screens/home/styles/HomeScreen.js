65a7c2c956510d366779b50ed22a91de7fe6d2be
manojmishra571980 <manojmishra571980@gmail.com>
2018-04-30 18:43:51 +1000

    common/loadingscreen working, styles in homescreen working

--- src/screens/home/styles/HomeScreen.js
+++ src/screens/home/styles/HomeScreen.js
@@ -1,11 +1,11 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-const styles = EStyleSheet.create({
-  root: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '$blackBlueColor',
-  },
+const styles = EStyleSheet.create(
+{  root: { flex: 1,  justifyContent: 'center', backgroundColor: '$blackBlueColor', },
+topContainer: {  flex: 1,  justifyContent: 'center',  alignItems: 'center',   backgroundColor: 'red'
+             },
+bottomContainer: {  flex: 0.8,  backgroundColor: 'blue'
+                  }
 });
 
 export default styles;
