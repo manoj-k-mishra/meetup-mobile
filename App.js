dbde837842eaf69c40bd6a976e0bfb9abf350cf8
manojmishra571980 <manojmishra571980@gmail.com>
2018-05-01 12:11:47 +1000

    fonts added - app working

--- App.js
+++ App.js
@@ -2,12 +2,28 @@ import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import Colors1 from './constants/Colors';
 import { HomeScreen } from './src/screens'
+//import { cachedFonts } from './helpers';
+import { fontAssets } from './helpers';
+import Expo, { AppLoading } from 'expo';
 
 EStyleSheet.build(Colors1);
 
 
 export default class App extends React.Component 
 {    
+  state = {    fontLoaded: false,}
+  componentDidMount() {  this._loadAssetsAsync();}
 
-render() { return <HomeScreen/> ; }
+ async _loadAssetsAsync() {
+  await Promise.all(fontAssets);
+
+  this.setState({ fontLoaded: true });
+}
+
+  render() 
+  {  if (!this.state.fontLoaded) 
+       {    return <AppLoading />;     }
+    
+    return <HomeScreen/> ; 
+  }
 }
--- src/screens/home/components/styles/MyMeetupsList.js
+++ src/screens/home/components/styles/MyMeetupsList.js
@@ -2,15 +2,20 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 
 const styles = EStyleSheet.create(
 {  root: {    flex: 1,  },
-   meetupCard: {  height: 200,  width: 175,  
-                 // marginHorizontal: 2% , 
-                 marginRight: 2,
-                 marginLeft:2,
-                backgroundColor: '#f73859',
-            
-},
-  
+   meetupCard: {  height: 200,  width: 175,   marginRight: 2, marginLeft:2, backgroundColor: '#f73859', },
+   meetupCardTopContainer: { flex: 1, position: 'relative',  },
+  meetupCardTitle: {  fontFamily: 'montserratBold', position: 'absolute',  color: '$whiteColor',  top: '2%',  left: '2.5%',  },
 
+  titleContainer: {    flex: 0.1, paddingHorizontal: '2.5%',  paddingVertical: '2.5%', },
+  title: {    color: '$whiteColor',    fontSize: 25, fontFamily: 'montserrat', },
+  contentContainer: {    flex: 1,},
+
+  meetupCardBottomContainer: {    flex: 0.4,    backgroundColor: '$whiteColor',    justifyContent: 'center',
+                                paddingHorizontal: '2.5%',
+                            },
+
+  meetupCardMetaName: { fontSize: 15, fontFamily: 'montserrat', },
+  meetupCardMetaDate: { fontSize: 13, fontFamily: 'montserratLight', },
   
 });
 
