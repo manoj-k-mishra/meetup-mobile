587a05bd418a97026986bae2ba0a86aaaf457257
manojmishra571980 <manojmishra571980@gmail.com>
2018-05-01 13:07:47 +1000

    react-navigation added- home screen working

---
+++ src/routes/HomeNavigator.js
@@ -0,0 +1,10 @@
+import { TabNavigator } from 'react-navigation';
+import React from 'react';
+import {    HomeScreen,
+
+       } from '../screens';
+
+  export default TabNavigator(
+      {    Home: {  screen: HomeScreen, }
+    });
+  
--- src/screens/home/components/MyMeetupsList.js
+++ src/screens/home/components/MyMeetupsList.js
@@ -3,35 +3,26 @@ import { View, Text, ScrollView } from 'react-native';
 import styles from './styles/MyMeetupsList';
 
 const MyMeetupsList = ({ meetups }) => (
-  <View style={styles.root}>
+ <View style={styles.root}>
     <View style={styles.titleContainer}>
       <Text style={styles.title}>My Meetups</Text>
     </View>
-<View style={styles.contentContainer}>
-      <ScrollView horizontal>
-        {meetups.map((meetup, i) => (
-          <View key={i} style={styles.meetupCard}>
-            <View style={styles.meetupCardTopContainer}>
-              <Text style={styles.meetupCardTitle}>
-                {meetup.title}
-              </Text>
-            </View>
-
-            <View style={styles.meetupCardBottomContainer}>
-              <Text style={styles.meetupCardMetaName}>
-                {meetup.group.name}
-              </Text>
-              <Text style={styles.meetupCardMetaDate}>
-                Mar 2m 6:00pm
-              </Text>
-            </View>
-
-          </View>
-        ))}
-      </ScrollView>
-</View>
-
-   </View>
+    <View style={styles.contentContainer}>
+        <ScrollView horizontal>
+            {meetups.map((meetup, i) => 
+            (   <View key={i} style={styles.meetupCard}>
+                    <View style={styles.meetupCardTopContainer}>
+                        <Text style={styles.meetupCardTitle}> {meetup.title} </Text>
+                    </View>
+                <View style={styles.meetupCardBottomContainer}>
+                    <Text style={styles.meetupCardMetaName}>  {meetup.group.name}  </Text>
+                    <Text style={styles.meetupCardMetaDate}>  Mar 2m 6:00pm  </Text>
+                </View>
+                </View>
+            ))}
+        </ScrollView>
+    </View>
+ </View>
         
  
 );
