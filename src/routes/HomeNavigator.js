9d2baadc0834c966d1d5e90e9531bf64fd631939
manojmishra571980 <manojmishra571980@gmail.com>
2018-05-05 11:16:33 +1000

    navigation icons with label working- all3

--- src/routes/HomeNavigator.js
+++ src/routes/HomeNavigator.js
@@ -1,12 +1,55 @@
 import { TabNavigator } from 'react-navigation';
 import React from 'react';
-import {    HomeScreen, NotificationsScreen, ProfileScreen
+import styled from 'styled-components/native';
+import Colors from '../../constants/Colors';
+import { MaterialIcons, FontAwesome } from '@expo/vector-icons';
+import Touchable from '@appandflow/touchable';
+import {    HomeScreen, NotificationsScreen, ProfileScreen } from '../screens';
 
-       } from '../screens';
+const AddButton = styled(Touchable)` marginRight: 10;`;
+
+const NavbarDefaultStyle = { backgroundColor: Colors.redColor, };
 
   export default TabNavigator(
-      {    Home: {  screen: HomeScreen, },
-           Notifications: {  screen: NotificationsScreen,},
-           ProfileScreen: {  screen: ProfileScreen,}
-    });
-  
+      {     Home: {  screen: HomeScreen,
+                    navigationOptions: ({ navigation }) => (
+                     {   headerStyle: NavbarDefaultStyle,
+                         headerRight: ( <AddButton feedback="opacity" onPress={() => navigation.navigate('CreateMeetup')}>
+                                     <MaterialIcons  name="add-circle"  size={30}  color="#fff" />
+                                </AddButton>
+                             ),
+                        tabBarIcon: ({ tintColor }) => ( <FontAwesome  name="home" size={25} color={tintColor} />   ),
+                     }),
+                },
+  //----------home screen done
+         Notifications: 
+         {   screen: NotificationsScreen,
+             navigationOptions: { headerStyle: NavbarDefaultStyle,
+                             tabBarIcon: ({ tintColor }) => ( <MaterialIcons name="notifications"  size={25} color={tintColor}   /> ),
+                            },
+        },
+        ProfileScreen: 
+          { screen: ProfileScreen,
+              navigationOptions: { headerStyle: NavbarDefaultStyle,
+                           tabBarIcon: ({ tintColor }) => ( <MaterialIcons name="account-circle" size={25} color={tintColor} />  ),
+                         },
+         },
+    },
+
+     {
+     //   swipeEnabled: false,
+     //   animationEnabled: false,
+        tabBarPosition: 'bottom',
+        tabBarOptions: 
+        {
+       //   showLabel: false,
+          showIcon: true,
+        //  inactiveTintColor: Colors.blackBlueColor,
+         // activeTintColor: Colors.redColor,
+       //   pressColor: Colors.redColor,
+        //  indicatorStyle: { backgroundColor: Colors.whiteColor },
+        //  style: {    backgroundColor: Colors.redColor, },
+        },
+     }
+);
+
