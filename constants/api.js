1d28931ff9c2c1ab8cbdeb5bec04992a64769751
manojmishra571980 <manojmishra571980@gmail.com>
2018-05-06 19:31:53 +1000

    actions reducer started for homescreen

--- constants/api.js
+++ constants/api.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 //import { Platform } from 'react-native';
 
 //axios.defaults.baseURL= 'http://10.133.44.111:3000/api';
-axios.defaults.baseURL= 'http://192.168.0.2:3000/api';
+axios.defaults.baseURL= 'http://192.168.0.3:3000/api';
 //axios.defaults.baseURL = url;
 const fakeGroupId = '5ae54364b713353104815402';
 class MeetupApi 
@@ -14,9 +14,10 @@ class MeetupApi
                     this.path = `/groups/${this.groupId}/meetups`;
                  }
    async fetchGroupMeetups() 
-   {     const { data } = await axios.get(this.path);
+   {     try {  const { data } = await axios.get(this.path);
                  return data.meetups;
+             } catch (e) {    console.log('/constants/api.js--e-',e);}
          
     }
-}
+} 
 export { MeetupApi,};
