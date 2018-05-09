52fcd9c7f115faf28a4f3b68a3c5cb7de7e8565b
manoj-k-mishra <manojmishra571980@gmail.com>
2018-05-09 10:41:23 +1000

    save meetup api working with createmeetupscreen

--- constants/api.js
+++ constants/api.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 //import { Platform } from 'react-native';
 
 axios.defaults.baseURL= 'http://10.133.44.111:3000/api';
-//axios.defaults.baseURL= 'http://192.168.0.2:3000/api';
+//axios.defaults.baseURL= 'http://192.168.0.5:3000/api';
 //axios.defaults.baseURL = url;
 const fakeGroupId = '5ae54364b713353104815402';
 class MeetupApi 
@@ -16,8 +16,19 @@ class MeetupApi
    async fetchGroupMeetups() 
    {     try {  const { data } = await axios.get(this.path);
                  return data.meetups;
-             } catch (e) {    console.log('/constants/api.js--e-',e);}
+             } catch (e) {    console.log('/constants/api.js-fetchGroupMeetups-e-',e);}
          
     }
+
+    async createGroupMeetups(args) 
+    {  try { console.log('/constatns/api.js-createGroupMeetups-args=',args.title);
+              const res = await axios.post(`${this.path}/new`,{ ...args });
+              console.log('/constatns/api.js-createGroupMeetups-res=',res);
+          return res;
+        } catch (e) { console.log('/constants/api.js-createGroupMeetups-e=',e);
+          //throw e;
+        }
+      }
+   
 } 
 export { MeetupApi,};
--- src/screens/createMeetup/CreateMeetupScreen.js
+++ src/screens/createMeetup/CreateMeetupScreen.js
@@ -5,6 +5,9 @@ import styles from './styles/CreateMeetupScreen';
 import Colors from '../../../constants/Colors';
 import DateTimePicker from 'react-native-modal-datetime-picker';
 import moment from 'moment';
+import {MeetupApi} from '../../../constants/api';
+
+const meetupApi=new MeetupApi();
 
 class CreateMeetupScreen extends Component 
 {  // static navigationOptions={title:'Create a new Meetup'}
@@ -17,13 +20,22 @@ class CreateMeetupScreen extends Component
                       return 'Pick a meetup date';
                    }
     _checkIfButtonSubmitDisabled() {    const { title, description, date } = this.state;
-                                         if (title.length > 5 && description.length > 5 && date > moment()) 
+                                         if (title.length > 5 && description.length > 10 && date > moment()) 
                                          {  return false;    }
                                          return true;
                                      }
     _changeTitle=title=>this.setState({title})
-    _changeDescription=description=>this.setState({description})
-    render() { console.log('/src/screens/createmeetup/createmeetupscreenjs-this.state=.',this.state);
+    _changeDescription = description=>this.setState({description})
+    _createMeetup = async () => { const { title, description, date } = this.state;
+                                const res=await meetupApi.createGroupMeetups({
+                                    title,
+                                    description,
+                                    date});
+                                console.log('/src/screens/createmeetup/createmeetupscreenjs-_createMeetup-res=',res)
+
+    }
+
+    render() { console.log('/src/screens/createmeetup/createmeetupscreenjs-render-this.state=.',this.state);
                
              return(
                     <View style={styles.root}>
@@ -53,6 +65,7 @@ class CreateMeetupScreen extends Component
                                          raised 
                                          fontFamily="montserrat"
                                          disabled={this._checkIfButtonSubmitDisabled()}
+                                         onPress={this._createMeetup}
                                 />
                            </View>
                       </View>
