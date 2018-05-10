93e6e0bdec9db977746a4610e7c767343763adfd
manoj-k-mishra <manojmishra571980@gmail.com>
2018-05-10 14:18:18 +1000

    styled-component installed and used on loginpage- first attempt

--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-//import styled from 'styled-components/native';
+import styled from 'styled-components/native';
 
-
+const FlexContainer = styled.View`
+  flex: 1;
+  justifyContent: center;
+  alignItems: center;
+  alignSelf: stretch;
+`;
 
 export default class LoginScreen extends Component 
 {   state = {};
-    render(){   return (<View>
+    render(){   return (<FlexContainer>
                           <Text>Login Page</Text>
-                        </View>
+                        </FlexContainer>
                       );
            }
 }
