5031110d148c5d53677871b9ac39b7840c919cdd
manoj-k-mishra <manojmishra571980@gmail.com>
2018-05-10 14:52:12 +1000

    login page created with fonts

--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import styled from 'styled-components/native';
+import Fonts from '../../../constants/Fonts';
+import Colors from '../../../constants/Colors';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const FlexContainer = styled.View`
-  flex: 1;
-  justifyContent: center;
-  alignItems: center;
-  alignSelf: stretch;
-`;
+const FlexContainer = styled.View`flex: 1;  justifyContent: center;  alignItems: center;  alignSelf: stretch;`;
+
+const MeetupText = styled.Text` color: ${Colors.$redColor};  fontSize: 18;  fontFamily: montserratBold;`;
+
+const BottomButtonWrapper = styled.View`  flex: 0.2;  flexDirection: row;`;
+
+const Button = styled.TouchableOpacity`  justifyContent: space-around;  alignItems: center;  flex: 1;  backgroundColor: ${({ color }) => color};
+  flexDirection: row;  paddingHorizontal: 10;`;
 
 export default class LoginScreen extends Component 
 {   state = {};
     render(){   return (<FlexContainer>
-                          <Text>Login Page</Text>
-                        </FlexContainer>
+                          <FlexContainer>
+                            <Text style={Fonts.authTitle}>Meetup Me</Text>
+                          </FlexContainer>
+                          <FlexContainer>
+                            <FlexContainer>
+                              <FlexContainer>
+                                <Text style={Fonts.authWelcomeTitle}>Welcome!</Text>
+                              </FlexContainer>
+                              <FlexContainer>
+                                <Text style={Fonts.authWelcomeText}>  Start managing your  {' '} 
+                                    <MeetupText>Meetups</MeetupText>  {' '} quickly and efficently
+                                </Text>
+                              </FlexContainer>
+                            </FlexContainer>
+                            <BottomButtonWrapper>
+                              <Button  color="#db3236"  >
+                                <Text style={Fonts.buttonAuth}>Connect with</Text>
+                                <MaterialCommunityIcons name="google" size={30} color="#fff" />
+                              </Button>
+                              <Button  color="#3b5998" >
+                                <Text style={Fonts.buttonAuth}>Connect with</Text>
+                                <MaterialCommunityIcons name="facebook" size={30} color="#fff" />
+                              </Button>
+                            </BottomButtonWrapper>
+                          </FlexContainer>
+                      </FlexContainer>
                       );
            }
 }
